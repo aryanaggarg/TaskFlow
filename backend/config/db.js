b538c0ad597914ee6d07c1f335ef6ddc690e15f8
Aryan Garg <aryanakgarg@gmail.com>
2025-07-20 19:42:19 +0530

    changed backend for vercel

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,10 +7,16 @@ const { MONGO_USER, MONGO_PASS, MONGO_CLUSTER, MONGO_DB } = process.env;
 const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_CLUSTER}/${MONGO_DB}?retryWrites=true&w=majority`;
 
 export const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+
   try {
-    await mongoose.connect(uri);
-    console.log('DB CONNECTED');
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+console.log('MongoDB Connected');
   } catch (err) {
-    console.error('DB CONNECTION ERROR:', err);
+    console.error('MongoDB Connection Error:', err);
+    throw err;
   }
 };
